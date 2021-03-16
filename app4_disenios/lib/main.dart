f60b4366803cbf85366d40dde1a227b78202823c
Ivan V <ivann96@gmail.com>
2021-03-16 08:42:25 -0300

    138. gradientes, rotacion

--- app4_disenios/lib/main.dart
+++ app4_disenios/lib/main.dart
@@ -1,19 +1,29 @@
 import 'package:app4_disenios/src/pages/basico_page.dart';
+import 'package:app4_disenios/src/pages/botones_page.dart';
 import 'package:app4_disenios/src/pages/scroll_page.dart';
-import 'package:flutter/material.dart'; 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; 
 void main() => runApp(MyApp());
  
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.light.copyWith(
+        statusBarColor: Colors.white
+      ),
+      
+    );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'intermedio',
+      initialRoute: 'avanzado',
       routes: {
         'basico': (BuildContext context) => BasicoPage(),
         'intermedio': (BuildContext context) => ScrollPage(),
-        //'avanzado': (BuildContext context) => AvanzadoPage(),
+        'avanzado': (BuildContext context) => BotonesPage(),
       },
     );
   }
