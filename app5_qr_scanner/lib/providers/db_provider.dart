eae72ca9108dd90d188ad46d0316561645b6dff1
Ivan V <ivann96@gmail.com>
2021-03-17 15:56:59 -0300

    162. db sqlite abm

--- app5_qr_scanner/lib/providers/db_provider.dart
+++ app5_qr_scanner/lib/providers/db_provider.dart
@@ -1,4 +1,7 @@
+import 'package:app5_qr_scanner/models/scan_model.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
 class DBPRovider {
   static Database _database;
@@ -15,7 +18,93 @@ class DBPRovider {
     return _database;
   }
 
-  Future<Database> initDB() {
+  Future<Database> initDB() async {
+    //Ruta donde se va almacenar la db
+    var documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, 'scans.db');
+    print(path);
 
+    //Creacion de la db
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute('''
+          CREATE TABLE scans(
+            id INTEGER PRIMARY KEY,
+            tipo TEXT,
+            valor TEXT
+          );
+        ''');
+      },
+    );
+  }
+
+  nuevoScanRaw(ScanModel nuevoScan) async {
+    final id = nuevoScan.id;
+    final tipo = nuevoScan.tipo;
+    final valor = nuevoScan.valor;
+    final db = await database;
+    final resp = await db.rawInsert('''
+      INSERT INTO scans(id, tipo, valor)
+        VALUES ($id, '$tipo', '$valor')
+    ''');
+
+    return resp;
+  }
+
+  nuevoScan(ScanModel nuevoScan) async {
+    final db = await database;
+    final resp = await db.insert('scans', nuevoScan.toJson());
+
+    return resp;
+  }
+
+  Future<ScanModel> obtenerScan(int id) async {
+    final db = await database;
+    final resp = await db.query('scans', where: 'id = ?', whereArgs: [id]);
+
+    return resp.isNotEmpty
+      ? ScanModel.fromJson(resp.first)
+      : null;
+  }
+
+  Future<List<ScanModel>> obtenerScansTipo(String tipo) async {
+    final db = await database;
+    final resp = await db.query('scans', where: 'tipo = ?', whereArgs: [tipo]);
+
+    return resp.isNotEmpty
+      ? resp.map((e) => ScanModel.fromJson(e)).toList()
+      : null;
+  }
+  
+  Future<List<ScanModel>> obtenerScansTodos() async {
+    final db = await database;
+    final resp = await db.query('scans');
+
+    return resp.isNotEmpty
+      ? resp.map((e) => ScanModel.fromJson(e)).toList()
+      : null;
+  }
+
+  Future<int> actualizarScan(ScanModel scan) async {
+    final db = await database;
+    final resp = await db.update('scans', scan.toJson(), where: 'id = ?', whereArgs: [scan.id]);
+
+    return resp;
+  }
+
+  Future<int> deleteScan(int id) async {
+    final db = await database;
+    final resp = await db.delete('scans', where: 'id = ?', whereArgs: [id]);
+
+    return resp;
+  }
+
+  Future<int> deleteAllScans() async {
+    final db = await database;
+    final resp = await db.delete('scans');
+
+    return resp;
   }
 }
--- app5_qr_scanner/lib/widgets/scan_button.dart
+++ app5_qr_scanner/lib/widgets/scan_button.dart
@@ -1,3 +1,5 @@
+import 'package:app5_qr_scanner/models/scan_model.dart';
+import 'package:app5_qr_scanner/providers/db_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
@@ -7,14 +9,19 @@ class ScanButton extends StatelessWidget {
     return FloatingActionButton(
       onPressed: () {
         //FlutterBarcodeScanner.getBarcodeStreamReceiver("#3d8bef", "Cancelar", false, ScanMode.QR).listen((qrResp) => _procesarQR());
-        var qrResp = 'pagina.com';
+        var qrResp = 'pagina.com.ar';
         _procesarQR(qrResp);
       },
       child: Icon(Icons.filter_center_focus),
     );
   }
 
-  _procesarQR(qrResp) {
+  _procesarQR(qrResp) async {
 
+    ScanModel test = ScanModel(id: 2, tipo: 'qwert', valor: qrResp);
+
+    //DBPRovider.db.nuevoScan(test);
+    var scan = await DBPRovider.db.obtenerScan(2);
+    print(scan.valor);
   }
 }
